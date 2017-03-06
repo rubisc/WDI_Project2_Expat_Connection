835a7efe1d6f26b059b9d3d7b682f5cc7eeeec8e
Rubi Cabral <rubisc@gmail.com>
2017-03-06 10:28:50 -0800

    fixed some validates and heroku bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :city, presence: true
   validates :body, presence: true, length: { maximum: 500 }
+  validates :date, presence: true
 end
